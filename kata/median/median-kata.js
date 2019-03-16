9a99b5d811ce4e0678b61e99ec397a281122c52e
Anuwong Mongkudkornburee <m.anuwong9@gmail.com>
2019-03-17 01:08:14 +0700

    Solved without refactoring

--- kata/median/median-kata.js
+++ kata/median/median-kata.js
@@ -1,6 +1,22 @@
 export const median = ({ numberArray }) => {
   numberArray.sort((a, b) => a - b);
   const length = numberArray.length;
-  const middleIndex = Math.floor(length / 2);
-  return (numberArray[middleIndex] + numberArray[length - 1 - middleIndex]) / 2;
+  const halfLength = length / 2;
+  const isOddNumberOfMember = length % 2 !== 0;
+  const isEvenNumberOfMember = length % 2 === 0;
+  const lastIndexOfFirstHalf = halfLength - 1;
+  const firstIndexOfSecondHalf = halfLength;
+  const middleIndex = Math.floor(halfLength);
+  const lastNumberOfFirstHalf = numberArray[lastIndexOfFirstHalf];
+  const firstNumberOfSecondHalf = numberArray[firstIndexOfSecondHalf];
+  const averageOfTwoValue =
+    (lastNumberOfFirstHalf + firstNumberOfSecondHalf) / 2;
+  if (length > 1) {
+    if (isOddNumberOfMember) {
+      return numberArray[middleIndex];
+    } else if (isEvenNumberOfMember) {
+      return averageOfTwoValue;
+    }
+  }
+  return numberArray[0];
 };
