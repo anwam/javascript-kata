0e2f1358fd09d82646486f1c2635674ed3740b75
Anuwong Mongkudkornburee <m.anuwong9@gmail.com>
2019-03-17 11:08:27 +0700

    Add 3 test cases
    - type validation
    - parameter range limitation

--- kata/ moore-neighbourhood/index.js
+++ kata/ moore-neighbourhood/index.js
@@ -1,4 +1,10 @@
 const findNeighbor = ({ grid, targetRow, targetColumn }) => {
+  if (!Array.isArray(grid[0]))
+    throw new TypeError('grid must be an ARRAY of tuples');
+  if (grid.length < 3)
+    throw new RangeError('grid length must greater or equal to 3');
+  if (grid.length > 10)
+    throw new RangeError('grid length must less or equal to 10');
   return {
     neighborsCount: 1,
   };
--- kata/ moore-neighbourhood/index.spec.js
+++ kata/ moore-neighbourhood/index.spec.js
@@ -1,12 +1,33 @@
 import findNeighbor from './index';
 
 describe('Find neighborhood cell', () => {
-  it('should return a number', () => {
-    const { neighborsCount } = findNeighbor({
-      grid: [],
-      targetRow: 0,
-      targetColumn: 0,
-    });
-    expect(neighborsCount).toEqual(1);
+  test('grid must be an ARRAY of tuples', () => {
+    expect(() => {
+      findNeighbor({
+        grid: [1, 2, 3],
+        targetRow: 0,
+        targetColumn: 0,
+      });
+    }).toThrow(new TypeError('grid must be an ARRAY of tuples'));
+  });
+
+  it('grid length must greater equal to 3', () => {
+    expect(() => {
+      findNeighbor({
+        grid: [[1]],
+        targetRow: 0,
+        targetColumn: 0,
+      });
+    }).toThrow(new RangeError('grid length must greater or equal to 3'));
+  });
+
+  it('grid length must less or equal to 10', () => {
+    expect(() => {
+      findNeighbor({
+        grid: [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11]],
+        targetRow: 0,
+        targetColumn: 0,
+      });
+    }).toThrow(new RangeError('grid length must less or equal to 10'));
   });
 });
